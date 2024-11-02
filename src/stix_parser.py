9c6fa0fef7a81d20590864e0cfa7cfe2252b2312
Vincenzo <32276363+vincenzocaputo@users.noreply.github.com>
2024-11-02 11:52:26 +0100

    Add config option to pull a specific mitre att&ck version

--- src/stix_parser.py
+++ src/stix_parser.py
@@ -8,13 +8,18 @@ class StixParser():
     Get and parse STIX data creating Tactics and Techniques objects
     Get the ATT&CK STIX data from MITRE/CTI GitHub repository. 
     Domain should be 'enterprise-attack', 'mobile-attack', or 'ics-attack'. Branch should typically be master.
+
     """
 
-    def __init__(self, repo_url, domain):
+    def __init__(self, repo_url, domain, version=None):
         self.url = repo_url
         self.domain = domain
 
-        stix_json = requests.get(f"{self.url}/{domain}/{domain}.json").json()
+        if version:
+            stix_json = requests.get(f"{self.url}/{domain}/{domain}-{version}.json").json()
+        else:
+            print(version)
+            stix_json = requests.get(f"{self.url}/{domain}/{domain}.json").json()
 
         self.src = MemoryStore(stix_data=stix_json['objects'])
 
